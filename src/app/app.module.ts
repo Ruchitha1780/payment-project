f4c438d2b28cd17d08d2ac0167c8b90e1e61fb9e
Ruchitha <ruchithakonduri@dbs.com>
2021-09-07 17:05:02 +0000

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+import { HeaderComponent } from './header/header.component';
+import { NavitemsComponent } from './navitems/navitems.component';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TransactionComponent } from './transaction/transaction.component';
+import { AccountComponent } from './account/account.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+LoginComponent,
+HeaderComponent,
+NavitemsComponent,
+TransactionComponent,
+AccountComponent
+],
+imports: [
+BrowserModule,
+FormsModule,
+ReactiveFormsModule,
+RouterModule.forRoot([
+{
+path:"login",component:LoginComponent
+},
+{
+path:"transaction", component:TransactionComponent
+},
+{
+path:"account", component:AccountComponent
+},
+{
+path:'', component:LoginComponent
+}
+
+ ])
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
